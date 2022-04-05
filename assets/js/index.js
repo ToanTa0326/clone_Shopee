01793f136c438cf094980890a91cd02f4438ed26
Tạ Quang Toàn <toanta0326@gmail.com>
2022-04-05 10:36:09 +0700

    responsive DONE!

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,13 @@ const modalInner = document.querySelector('.modalinner');
 const register = document.querySelector('.formRegister');
 const login = document.querySelector('.formLogin');
 const switchBtn = document.querySelectorAll('.authForm__switch-btn');
+const searchBtn = document.querySelector('.header__mobile-search')
+const headerSearch = document.querySelector('.header__search');
+const headerSearchInput = document.querySelector('.header__search-input')
+const headerCart = document.querySelector('.header__cart');
+const listCart = document.querySelector('.header__cart-list')
+const searchSelect = document.querySelector('.header__search-select')
+const searchOption = document.querySelector('.header__search-option')
 
 switchBtn.forEach((btn,id) => {
     btn.addEventListener('click', () => {
@@ -45,4 +52,20 @@ modal.addEventListener('click', () => {
 
 modalInner.addEventListener('click', (e) => {
     e.stopPropagation()
+})
+
+searchBtn.addEventListener('click', () => {
+    headerSearch.classList.toggle('open');
+})
+
+headerSearchInput.addEventListener('blur', () => {
+    headerSearch.classList.remove('open')
+})
+
+headerCart.addEventListener('click', () => {
+    listCart.classList.toggle('open')
+})
+
+searchSelect.addEventListener('click', () => {
+    searchOption.classList.toggle('open')
 })
